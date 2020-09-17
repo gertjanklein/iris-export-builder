c2bc6f8f344fe1befecfa1ca5aaa7fb4f6a23380
Gertjan Klein <gklein@xs4all.nl>
2020-09-17 11:18:02 +0200

    Additional test for set_in_path

--- tests/test_namespace.py
+++ tests/test_namespace.py
@@ -6,10 +6,15 @@ def test_set_in_path():
     ns.set_in_path(cfg, 'Testing.One.Two.Three.name', '42')
     assert cfg.Testing.One.Two.Three.name == '42'
 
-def test_set_in_path_error():
+def test_set_in_path_section_is_value():
     cfg = ns.Namespace()
     cfg.Section = ''
     with pytest.raises(ns.ConfigurationError):
         ns.set_in_path(cfg, "Section.name", '42')
 
+def test_set_in_path_section_exists():
+    cfg = ns.Namespace()
+    cfg.Section = ns.Namespace()
+    ns.set_in_path(cfg, "Section.name", '42')
+    assert cfg.Section.name == '42'
 
