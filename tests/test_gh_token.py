ff3357abd9bf3c0c36cb359383b43051b29ec06e
Gertjan Klein <gklein@xs4all.nl>
2021-08-03 15:55:26 +0200

    Add test for loading token from file

---
+++ tests/test_gh_token.py
@@ -0,0 +1,67 @@
+""" Tests loading a token from an external file.
+"""
+
+from pathlib import Path
+
+import pytest
+
+import namespace as ns
+
+
+# Basic configuration for the tests in this module
+CFG = """
+[Source]
+type = "github"
+[GitHub]
+user = 'x'
+repo = 'x'
+tag = 'x'
+token = '{token}'
+[Local]
+outfile = 'out.xml'
+"""
+
+
+@pytest.mark.usefixtures("reload_modules")
+def test_token_embedded(tmp_path:Path, get_config):
+    """ Tests token directyly in configuration """
+
+    cfg = CFG.format(token='abc')
+    config = get_config(cfg, tmp_path) # type: ns.Namespace
+    assert 'GitHub' in config, "No Server section"
+    gh = config.GitHub
+    assert isinstance(gh, ns.Namespace), "GitHub not a section"
+    assert gh.token == 'abc', f"Token has unexpected value: '{gh.token}'"
+
+
+@pytest.mark.usefixtures("reload_modules")
+def test_token_external_no_nl(tmp_path:Path, get_config):
+    """ Tests token from external file """
+
+    ext = tmp_path / 'token.txt'
+    with open(ext, 'wt') as f:
+        f.write('def')
+
+    cfg = CFG.format(token=f"@{ext}")
+    config = get_config(cfg, tmp_path) # type: ns.Namespace
+    assert 'GitHub' in config, "No Server section"
+    gh = config.GitHub
+    assert isinstance(gh, ns.Namespace), "GitHub not a section"
+    assert gh.token == 'def', f"Token has unexpected value: '{gh.token}'"
+
+
+@pytest.mark.usefixtures("reload_modules")
+def test_token_external_with_nl(tmp_path:Path, get_config):
+    """ Tests token from external file """
+
+    ext = tmp_path / 'token.txt'
+    with open(ext, 'wt') as f:
+        f.write('ghi\n\n\n')
+
+    cfg = CFG.format(token=f"@{ext}")
+    config = get_config(cfg, tmp_path) # type: ns.Namespace
+    assert 'GitHub' in config, "No Server section"
+    gh = config.GitHub
+    assert isinstance(gh, ns.Namespace), "GitHub not a section"
+    assert gh.token == 'ghi', f"Token has unexpected value: '{gh.token}'"
+
