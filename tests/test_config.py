d78336a17445d4269fb271e85eabd36316656a4f
Gertjan Klein <gklein@xs4all.nl>
2021-08-07 12:49:53 +0200

    Add tests for usage message

--- tests/test_config.py
+++ tests/test_config.py
@@ -115,3 +115,60 @@ def test_CSPparsers_section_check(tmp_path):
         msg:str = e.value.args[0]
         assert msg.startswith("At least one [[CSP.parsers]] section"), f"Unexpected message {msg}"
 
+
+@pytest.mark.usefixtures("reload_modules")
+def test_usage_msg():
+    """ Tests that a usage message is displayed when no arguments supplied
+    
+    Not specifying anything on the commandline is an error, so the error
+    flag to msgbox should be set, and the exit code should be 2 (this is
+    determined by argparse).
+    """
+ 
+    args = ['builder']
+    seen = False
+    
+    # Replacement for msgbox that checks the message that would be displayed
+    def msgbox(msg, is_error=False):
+        nonlocal seen
+        seen = True
+        msg = msg.split('\n', maxsplit=1)[0]
+        assert msg.startswith("usage: builder"), f"Unexpected or missing usage message: {msg}"
+        assert is_error, "Error mode for msgbox not set."
+
+    # Pass a missing config file name to the configuration parser
+    with patch('sys.argv', args), patch('config.msgbox', msgbox):
+        with pytest.raises(SystemExit) as e:
+            cfg = config.get_config()
+        code = e.value.args[0]
+        assert code == 2, f"Exit code not 2 but {code}"
+        assert seen, "msgbox not called"
+    
+
+@pytest.mark.usefixtures("reload_modules")
+def test_help_msg():
+    """ Tests that a help message is displayed with argument --help
+    
+    This situation is not an error, so the error flag to msgbox should
+    not be set, and the exit code should be 0.
+    """
+ 
+    args = ['builder', '--help']
+    seen = False
+    
+    # Replacement for msgbox that checks the message that would be displayed
+    def msgbox(msg, is_error=False):
+        nonlocal seen
+        seen = True
+        msg = msg.split('\n', maxsplit=1)[0]
+        assert msg.startswith("usage: builder"), f"Unexpected or missing usage message: {msg}"
+        assert not is_error, "Error mode for msgbox set."
+
+    # Pass a missing config file name to the configuration parser
+    with patch('sys.argv', args), patch('config.msgbox', msgbox):
+        with pytest.raises(SystemExit) as e:
+            cfg = config.get_config()
+        code = e.value.args[0]
+        assert code == 0, f"Exit code not 0 but {code}"
+        assert seen, "msgbox not called"
+    
